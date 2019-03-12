2284f1ba1487c0cdebc5b7cc421fa3857c1384e7
HENRY <prez.gumiel@gmail.com>
2019-03-11 22:16:00 -0400

    corregido

--- footer.php
+++ footer.php
@@ -33,7 +33,6 @@
     
     <!-- Jquery validation JS-->
     <script type="text/javascript" src="public/libs/jquery-validation/js/jquery.validate.min.js"></script>
-    <script src="public/configurations/configurations.jquery.validate.js?date='<?php echo uniqid()?>'"></script>
 
     <!-- Select2 JS -->
     <script src="public/libs/select2/js/select2.min.js"></script>
@@ -48,6 +47,7 @@
     <script src="public/libs/serializejson/js/jquery.serializejson.min.js" type="text/javascript"></script>
 
     <!-- Configurations -->
+    <script src="public/configurations/config.jquery.validate.js?date='<?php echo uniqid()?>'"></script>
 
     <!-- Componentes -->
     <script src="public/components/ContainerJS.js" type="text/javascript"></script>
